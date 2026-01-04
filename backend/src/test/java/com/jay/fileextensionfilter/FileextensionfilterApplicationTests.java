c99455a073fcd2907d2be360c6a74b3ccbb0df5f
jay1261 <dongjae73@naver.com>
2026-01-04 16:43:21 +0900

    feat: initialize Spring Boot backend project

---
+++ backend/src/test/java/com/jay/fileextensionfilter/FileextensionfilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.fileextensionfilter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileextensionfilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
